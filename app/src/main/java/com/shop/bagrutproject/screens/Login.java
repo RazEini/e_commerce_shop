46d1e41187d2746d8d3c18d6e1909355887793b4
RazEini <razeini972@gmail.com>
2025-05-29 21:56:26 +0300

    eod 29/5/25

--- app/src/main/java/com/shop/bagrutproject/screens/Login.java
+++ app/src/main/java/com/shop/bagrutproject/screens/Login.java
@@ -92,7 +92,28 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         authenticationService = AuthenticationService.getInstance();
         databaseService = DatabaseService.getInstance();
 
+        checkIfUserIsAlreadyLoggedIn();
+
         initViews();
+    }
+
+    private void checkIfUserIsAlreadyLoggedIn() {
+        boolean isAdmin = SharedPreferencesUtil.isAdmin(this);
+        if (isAdmin) {
+            Log.d(TAG, "Admin is already logged in, redirecting...");
+            Intent adminIntent = new Intent(this, CategoriesActivity.class);
+            startActivity(adminIntent);
+            finish();
+            return;
+        }
+
+        if (authenticationService.isUserSignedIn()) {
+            Log.d(TAG, "User is already logged in, redirecting...");
+            SharedPreferencesUtil.setIsAdmin(this, false);
+            Intent go = new Intent(this, CategoriesActivity.class);
+            startActivity(go);
+            finish();
+        }
     }
 
     private void initViews() {
